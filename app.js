1edcaf2d6034a9d989af06af925ad3e714761e55
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-07-28 13:59:41 +0300

    building the api: Handling PATCH requests

--- app.js
+++ app.js
@@ -63,6 +63,20 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour: '<Updated tour here...>' },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
