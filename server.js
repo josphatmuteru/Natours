533746433b833145da5cc0d42a27f4a9c39b6c58
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-20 16:14:51 +0300

    creating documentts from the model

--- server.js
+++ server.js
@@ -31,6 +31,21 @@ const tourSchema = new mongoose.Schema({
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error:', err);
+  });
+
 // console.log(process.env);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
