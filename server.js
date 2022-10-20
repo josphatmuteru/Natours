3e27781b9b3c47a2c09640a8f446b6b3d6c2f53c
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-20 16:03:28 +0300

    creating a simple model using mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+const app = require('./app');
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -19,8 +20,17 @@ mongoose
   })
   .then((con) => console.log('DB connection successful'));
 
-const app = require('./app');
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+});
 
+const Tour = mongoose.model('Tour', tourSchema);
 // console.log(process.env);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
