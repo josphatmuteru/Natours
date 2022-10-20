2a61ad3fc06e87b6502cafc674985b962babac2b
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-20 15:45:41 +0300

    installed mongoose and learnt how to connect to database

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
-// const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// const DB =
+//   'mongodb+srv://Josphat:2UrHBrLjiuO2UI5h@cluster0.m0kdoqh.mongodb.net';
+
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => console.log('DB connection successful'));
 
 const app = require('./app');
 
