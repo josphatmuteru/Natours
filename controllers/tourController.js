80144fefb560fde6410b415380b6ba480e2aaa2d
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-24 22:52:18 +0300

    Implemented advanced filtering and sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,7 +9,32 @@ const Tour = require('./../models/tourModel.js');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILD QUERY
+    //1A) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    //1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+    // const tours = await Tour.find()
+    // .where('duration')
+    // .equals(5).where('difficulty')
+    // .equals('easy');
+
+    //2) SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //EXECUTE QUERY
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
