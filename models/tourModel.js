e0d0e15f4873e6380b4eebbb25ffa50644f14217
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-27 22:17:36 +0300

    Implementing query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -41,6 +41,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -53,18 +57,33 @@ tourSchema.virtual('durationWeeks').get(function () {
 });
 
 //DOCUMENT MIDDLEWARE: runs before .save() and .create()
-tourSchema.pre('save', function (next) {
-  this.slug = slugify(this.name, { lower: true });
+// tourSchema.pre('save', function (next) {
+//   this.slug = slugify(this.name, { lower: true });
+//   next();
+// });
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+//QUERY MIDDLEWARE
+// tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
   next();
 });
 
-tourSchema.pre('save', function (next) {
-  console.log('Will save document');
-  next();
-});
-
-tourSchema.post('save', function (doc, next) {
-  console.log(doc);
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
   next();
 });
 const Tour = mongoose.model('Tour', tourSchema);
