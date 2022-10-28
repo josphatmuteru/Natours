f106e3a00d82df111e8a8cdf9e5864ba9d136aa1
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-28 17:00:20 +0300

    Separating production and development errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,39 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    //1. Log error
+    console.error('ERROR :/', err);
+
+    //2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev();
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
