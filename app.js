bc2acde96fd3cf4cde5867ff4591c6010573a559
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-28 15:15:28 +0300

    Handling unhandled routes

--- app.js
+++ app.js
@@ -11,10 +11,12 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
+
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware 👋');
+//   next();
+// });
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
@@ -28,6 +30,13 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users/', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 // 4. START SERVER
 module.exports = app;
