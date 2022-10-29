d58ffd1575ca5404675495e8a39911ef041d7520
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-29 09:45:52 +0300

    Handling invalid  database IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('./../utils/appError');
+const handleCastErrorDB = (err) => {
+  const message = `Invalid${err.path}: ${err.value}`;
+
+  return new AppError(message, 404);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -32,8 +39,10 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev();
+    sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(err);
+    sendErrorProd(error, res);
   }
 };
