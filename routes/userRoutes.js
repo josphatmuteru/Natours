c3fac0b7840e75b64a8267d0f53acf8915d26faf
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-29 11:54:45 +0300

    Adding new users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-// const fs = require('fs');
 const userController = require('../controllers/userController');
+const authController = require('./../controllers/authController');
 const router = express.Router();
+
+router.post('/signup', authController.signup);
 
 router
   .route('/')
