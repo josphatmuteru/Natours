83851194311ab638d6c2588691d3330c79ee20db
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-29 16:17:39 +0300

    Protecting tour routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,3 +48,25 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  //1) Getting token and checking if its there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+  if (!token) {
+    return next(
+      new AppError('You are not Logged in! Please log in to access.', 401)
+    );
+  }
+  //2) Verification token
+  //3) Check if user still exists
+  //4) Check if user changed password after the token was issued
+
+  next();
+});
