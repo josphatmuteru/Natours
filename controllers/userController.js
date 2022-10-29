313627a56db45b934a17125b7cd16743928d5662
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-10-29 15:39:23 +0300

    Logging in users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const User = require('./../models/UserModel');
+const catchAsync = require('./../utils/catchAsync');
 
-exports.getAllUsers = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'error', message: 'This route is not defined' });
-};
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
 
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
 exports.getUser = (req, res) => {
   res
     .status(500)
--- models/userModel.js
+++ models/userModel.js
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minLength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -44,5 +45,14 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+// const User = mongoose.model('User', userSchema);
+// module.exports = User;
+
+module.exports = mongoose.models.User || mongoose.model('User', userSchema);
