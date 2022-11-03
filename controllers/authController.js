be7d0a1d234940e3a495399f4cf0273bc323b6ac
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-11-03 16:48:24 +0300

    Sending a jwt through a cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,6 +9,18 @@ const sendEmail = require('./../utils/email');
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user.id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  //Remove password from output
+  user.password = undefined;
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
   res.status(statusCode).json({
     status: 'success',
     token,
@@ -17,6 +29,7 @@ const createSendToken = (user, statusCode, res) => {
     },
   });
 };
+
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
@@ -27,6 +40,7 @@ exports.signup = catchAsync(async function (req, res, next) {
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
+    role: req.body.role,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
     passwordChangedAt: req.body.passwordChangedAt,
