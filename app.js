2b53bdce489ccf187b9a1853709e59e99a58efc6
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-11-04 20:51:08 +0300

    Setting security headers and performing data sanitization

--- app.js
+++ app.js
@@ -2,33 +2,53 @@ const express = require('express');
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const rateLimit = require('express-rate-limit');
+// const helmet = require('helmet')
+// const mongoSanitize = require('express-mongo-sanitize');
+// const xss = require('xss-clean');
+
 const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
 
 // 1: GLOBAL MIDDLEWARE
-// console.log(process.env.NODE_ENV);
+//Set security HTTP headers
+// npm i helmet
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+//Limit requests from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP try again in an hour',
 });
-
 app.use('/api', limiter);
-app.use(express.json());
+
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NoSQL query injection
+// npm i express-mongo-sanitize
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+// npm i npm xss-clean
+app.use(xss());
 
 // app.use((req, res, next) => {
 //   console.log('Hello from the middleware 👋');
 //   next();
 // });
 
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
