f457c2d7fa8ebd047cb90c2e9ad63dc862dd3de2
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-11-10 22:11:24 +0300

    Adding missing authentication and authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,19 +7,21 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch(
-  '/updateMyPassword/',
-  authController.protect,
-  authController.updatePassword
-);
-router.patch('/updateMe/', authController.protect, userController.updateMe);
-router.delete('/deleteMe/', authController.protect, userController.deleteMe);
+
+// Protect all routes after this middelware
+router.use(authController.protect);
+
+router.patch('/updateMyPassword/', authController.updatePassword);
+router.patch('/updateMe/', userController.updateMe);
+router.delete('/deleteMe/', userController.deleteMe);
 router.get(
   '/me',
-  authController.protect,
+
   userController.getMe,
   userController.getUser
 );
+
+router.use(authController.restrictTo('admin'));
 router
   .route('/')
   .get(userController.getAllUsers)
