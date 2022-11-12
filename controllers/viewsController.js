1657758f60834a3b69eb58f07747b4e464251768
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-11-12 10:16:24 +0300

    Building the tour overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,15 +1,21 @@
 express = require('express');
-// const catchAsync = require('../utils/catchAsync');
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
-exports.getTour = (req, res) => {
+exports.getOverView = catchAsync(async (req, res) => {
+  //1) Get tour data from collection
+  const tours = await Tour.find();
+  //2)Build template
+  //3)Render that template using pug
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker',
   });
-};
-
-exports.getOverView = (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-};
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const router = express.Router();
 
-router.get('/', viewsController.getTour);
-router.get('/tour', viewsController.getOverView);
+router.get('/', viewsController.getOverView);
+router.get('/tour', viewsController.getTour);
 
 module.exports = router;
