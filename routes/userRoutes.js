fe62c94b0e2945020a489bbc9464223df1f0f548
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-11-21 16:41:53 +0300

    Implementing image uploads with multer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const multer = require('multer');
 const userController = require('../controllers/userController');
 const authController = require('./../controllers/authController');
+
+const upload = multer({ dest: 'public/img/users' });
 const router = express.Router();
 
 router.post('/signup', authController.signup);
@@ -13,7 +16,7 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 router.use(authController.protect);
 
 router.patch('/updateMyPassword/', authController.updatePassword);
-router.patch('/updateMe/', userController.updateMe);
+router.patch('/updateMe/', upload.single('photo'), userController.updateMe);
 router.delete('/deleteMe/', userController.deleteMe);
 router.get(
   '/me',
