5b81529b49e816dca51c0e03b784629a4e99835d
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-11-22 10:43:33 +0300

    Saving image name to database

--- controllers/userController.js
+++ controllers/userController.js
@@ -70,7 +70,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
   //2) Filter out unwanted fields
   const filteredBody = filterObj(req.body, 'name', 'email');
-
+  if (req.file) filteredBody.photo = req.file.filename;
   //3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
