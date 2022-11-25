a6b7f41b16368c574022247b5dd2f65fcdb32fbe
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2022-11-25 16:18:26 +0300

    Finishing the booking API

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -4,7 +4,7 @@ const Booking = require('../models/bookingModel');
 const user = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-
+const factory = require('../controllers/handlerFactory');
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
@@ -52,3 +52,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- models/bookingModel.js
+++ models/bookingModel.js
@@ -26,7 +26,7 @@ const bookingSchema = new mongoose.Schema({
 });
 
 bookingSchema.pre(/^find/, function (next) {
-  this.populate('user', {
+  this.populate('user').populate({
     path: 'tour',
     select: 'name',
   });
